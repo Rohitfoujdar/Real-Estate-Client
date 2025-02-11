d322ce7e1636b374073c554cc64fe69c169f68e3
Rohitfoujdar <rohitfoujdar8696@gmail.com>
2025-02-11 10:31:47 +0530

    ---done----

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API = "http://localhost:8000/"
+export const API = "https://real-estate-server-8d6o.onrender.com"
 export const GOOGLE_PLACES_KEY = "AIzaSyBsjVPC3VpGqj0GU6H1tuZ6AF0MR5Hh-q8"
